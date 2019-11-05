9748d4c845e5a0289f0b4d210a69169c936119d5
Marc Veens <Marc.Veens@macaw.nl>
2019-11-05 23:42:23 +0100

    Now using the production version instead of development

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 ![Travis CI](https://travis-ci.com/marcveens/e2e-ci-tests.svg?branch=master)
 
 ## How it works
-This repo makes it possible to run end-to-end tests on a build agent using your create-react-app localhost development environment. 
+This repo makes it possible to run end-to-end tests on a build agent using your create-react-app localhost production environment. 
 
 - e2e tests can be run either headful(?) and headless
 - In case the `npm run tests-e2e-ci` command runs on a build agent:
-    1. Node runs the `npm run ci-start` command (same as `npm start` but without opening an actual browser)
+    1. Node runs `npm run build`
+    2. Node runs `npm run serve` which serves the built app on port 3000.
     2. Node waits for port 3000 to become available. This means that the React app is running. 
     3. `npm run test-e2e-ci-exec` will run and use localhost:3000 for the e2e tests
-    4. When a test fails, the build will also fail
+    4. When a test fails, the build agent will also fail
     5. No matter what the result is, all started processes will be killed gracefully.
 
 ## Used packages for e2e testing
@@ -23,5 +24,5 @@ This repo makes it possible to run end-to-end tests on a build agent using your 
 | Command | Description |
 |-|-|
 | `test-e2e` | Runs E2E tests in non-headless mode, thus it opens a browser for you. |
-| `test-e2e-ci` | Runs E2E tests in a headless browser + runs `npm start` beforehand. |
-| `test-e2e-ci-exec` | Runs E2E tests in a headless browser. Requires you to run `npm start` beforehand. |
+| `test-e2e-ci` | Runs E2E tests in a headless browser + runs `npm build` and `npm run serve` beforehand. |
+| `test-e2e-ci-exec` | Runs E2E tests in a headless browser. Requires you to run `npm start` or `npm build` beforehand. |
--- tests/e2e/run-tests-ci.js
+++ tests/e2e/run-tests-ci.js
@@ -4,70 +4,76 @@ const kill = require('tree-kill');
 
 let activeProcesses = [];
 
-const waitOnReactOpts = { resources: ['http://localhost:3000'] };
-
 const runScript = (cmd, args, onSuccess) => {
-  let errorMessage = '';
-  const subprocess = spawn(cmd, args, { shell: true });
+    let errorMessage = '';
+    const subprocess = spawn(cmd, args, { shell: true });
 
-  activeProcesses.push(subprocess);
+    activeProcesses.push(subprocess);
 
-  subprocess.stdout.on('data', data => {
-    console.log(data.toString());
-  });
-
-  subprocess.stderr.on('data', data => {
-    errorMessage += data.toString();
-  });
-
-  subprocess.on('close', code => {
-    if (code !== 0) {
-      killProcesses(() => {
-        throw new Error(`Process ${cmd} ${args} threw an error:\n ${errorMessage}`);
-      });
-    } else {
-      if (onSuccess) {
-        onSuccess();
-      }
-    }
-  });
-
-  subprocess.on('error', err => {
-    killProcesses(() => {
-      throw new Error(`Failed to start process ${cmd} ${args}. Error: ${err.toString()}`);
+    subprocess.stdout.on('data', data => {
+        console.log(data.toString());
     });
-  });
 
-  return subprocess;
+    subprocess.stderr.on('data', data => {
+        errorMessage += data.toString();
+    });
+
+    subprocess.on('close', code => {
+        if (code !== 0) {
+            killProcesses(() => {
+                throw new Error(`Process ${cmd} ${args} threw an error:\n ${errorMessage}`);
+            });
+        } else {
+            if (onSuccess) {
+                onSuccess();
+            }
+        }
+    });
+
+    subprocess.on('error', err => {
+        killProcesses(() => {
+            throw new Error(`Failed to start process ${cmd} ${args}. Error: ${err.toString()}`);
+        });
+    });
 };
 
-const initialize = cb => {
-  console.log('Starting React host');
-  runScript('npm', ['run', 'ci-start']);
+const killProcesses = async (onKill) => {
+    let processesKilled = 0;
+
+    activeProcesses.forEach(process => {
+        if (process) {
+            kill(process.pid, () => {
+                processesKilled = processesKilled + 1;
+
+                if (processesKilled === activeProcesses.length && onKill) {
+                    onKill();
+                }
+            });
+        }
+    });
 };
 
-const killProcesses = cb => {
-  activeProcesses.forEach(process => {
-    if (process) {
-      kill(process.pid, cb);
-    }
-  });
+const initialize = async () => {
+    console.log('Starting React host');
+    runScript('npm', ['run', 'build'], () => {
+        runScript('npm', ['run', 'serve']);
+    });
+
+    waitOn({ resources: ['http://localhost:3000'] }, async err => {
+        if (err) {
+            console.err(err);
+            process.exit(1);
+        }
+        console.log('Port 3000 ready');
+
+        console.log('Start running e2e tests');
+        runScript('npm', ['run', 'test-e2e-ci-exec'], () => {
+            killProcesses(() => {
+                console.log('Succesfully ran all e2e tests');
+                process.exit(0);
+            });
+        });
+    });
 };
 
 initialize();
-
-waitOn(waitOnReactOpts, err => {
-  if (err) {
-    console.err(err);
-    process.exit(1);
-  }
-  console.log('Port 3000 ready');
-
-  console.log('Start running e2e tests');
-  runScript('npm', ['run', 'test-e2e-ci-exec'], () => {
-    killProcesses(() => {
-      console.log('Succesfully ran all e2e tests');
-      process.exit(0);
-    });
-  });
-});
